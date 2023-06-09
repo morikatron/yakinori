503ce39379419e7eab90475db0e14d3821896ac6
yamada-hikaru <hikaru.yamada@morikatron.co.jp>
2023-06-10 05:49:02 +0900

    first_commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yakinori',
+    version='0.1.0',
+    url='https://github.com/morikatron/yakinori',
+    author='Hikaru Yamada',
+    author_email='hikaru.yamada@morikatron.co.jp',
+    description='yakinori is a tool for converting Kanji to hiragana, katakana, roma-ji.',
+    packages=find_packages(),
+    install_requires=[],
+)
